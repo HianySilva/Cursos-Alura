6963afcf8d5d0f82eabbe15f5a52ad39a32f7719
HianySilva <hgfs@discente.ifpe.edu.br>
2024-10-30 08:24:45 -0300

    feat: dados.js

---
+++ dados.js
@@ -0,0 +1,20 @@
+let filmes = [
+    {
+        titulo: 'Simplesmente Acontece',
+        sinopse: ' Uma comédia romântica clássica sobre a amizade que se transforma em amor. A história de Rosie e Alex é marcada por altos e baixos, mas a amizade deles sempre prevalece.',
+        ano: 2014,
+        genero: 'Romance'
+      },
+     {
+        titulo: 'A Cinco Passos de Você',
+        sinopse: 'Um drama romântico sobre dois adolescentes com fibrose cística que se apaixonam.',
+        ano: 2019,
+        genero: 'Drama'
+     },
+    {
+        titulo: 'Amor sem Fim',
+        sinopse: 'Remake de um clássico, contando a história de um jovem casal que enfrenta obstáculos para ficar juntos.',
+        ano: 2014,
+        genero: 'Romance'
+     }
+    ];
